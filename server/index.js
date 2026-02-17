7e05abf23dd1dbd7f9b6235e64af912b423dd1df
Ishita Kapoor <ishitakapoor12612@gmail.com>
2026-02-17 11:01:59 +0530

    Final code

--- server/index.js
+++ server/index.js
@@ -1,25 +1,18 @@
-const express = require('express')
+const express = require("express");
 require("dotenv").config();
-const cors = require("cors")
-const path = require("path")
-const connectDb = require("./config/db")
-const app = express()
+const cors = require("cors");
+const path = require("path");
+const connectDb = require("./config/db");
+const app = express();
 connectDb();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client")))
-const port= process.env.PORT || 5000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.use("/api/users", require("./routes/userRoutes"));
 
-// app.get('/api/users', usersHandler)
-
-// const usersHandler = (req, res) => {
-//   res.send('Hello user!')
-// }
+app.use(express.static(path.join(__dirname, "../client")));
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
